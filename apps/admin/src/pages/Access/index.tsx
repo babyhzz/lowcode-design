85996e9bacbd7552a53546cde177b3c09a3e296e
hzz <383686357@qq.com>
2023-09-02 22:57:17 +0800

    feat: 集成渲染器

--- apps/admin/src/pages/Access/index.tsx
+++ apps/admin/src/pages/Access/index.tsx
@@ -1,20 +1,80 @@
-import { PageContainer } from '@ant-design/pro-components';
-import { Access, useAccess } from '@umijs/max';
-import { Button } from 'antd';
+import { render as renderAmis } from 'amis';
+import 'amis/lib/helper.css';
+import 'amis/lib/themes/cxd.css';
+import 'amis/sdk/iconfont.css';
+
+const schema = {
+  type: 'page',
+  body: [
+    {
+      label: '新增',
+      type: 'button',
+      actionType: 'dialog',
+      level: 'primary',
+      className: 'm-b-sm',
+      dialog: {
+        title: '新增表单',
+        body: {
+          type: 'form',
+          api: 'post:https://3xsw4ap8wah59.cfc-execute.bj.baidubce.com/api/amis-mock/mock2/sample',
+          body: [
+            {
+              type: 'input-text',
+              name: 'engine',
+              label: 'Engine',
+            },
+            {
+              type: 'input-text',
+              name: 'browser',
+              label: 'Browser',
+            },
+          ],
+        },
+      },
+    },
+    {
+      type: 'crud',
+      api: 'https://3xsw4ap8wah59.cfc-execute.bj.baidubce.com/api/amis-mock/mock2/sample?orderBy=id&orderDir=desc',
+      syncLocation: false,
+      columns: [
+        {
+          name: 'id',
+          label: 'ID',
+        },
+        {
+          name: 'engine',
+          label: 'Rendering engine',
+        },
+        {
+          name: 'browser',
+          label: 'Browser',
+        },
+        {
+          name: 'platform',
+          label: 'Platform(s)',
+        },
+        {
+          name: 'version',
+          label: 'Engine version',
+        },
+        {
+          name: 'grade',
+          label: 'CSS grade',
+        },
+      ],
+    },
+  ],
+};
 
 const AccessPage: React.FC = () => {
-  const access = useAccess();
   return (
-    <PageContainer
-      ghost
-      header={{
-        title: '权限示例',
-      }}
-    >
-      <Access accessible={access.canSeeAdmin}>
-        <Button>只有 Admin 可以看到这个按钮</Button>
-      </Access>
-    </PageContainer>
+    <div>
+      {renderAmis(schema, {
+        data: {
+          username: 'amis',
+        },
+      })}
+    </div>
   );
 };
 
