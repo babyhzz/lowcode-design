194cb0445d0b10ef43bd50ec684483b087c63e10
hzz <383686357@qq.com>
2023-09-10 18:19:09 +0800

    删除多余项目

--- apps/web-delete/.umirc.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'umi';
-
-export default defineConfig({
-  routes: [
-    { path: '/', component: 'index', layout: false },
-    { path: '/docs', component: 'docs' },
-  ],
-  npmClient: 'pnpm',
-});
