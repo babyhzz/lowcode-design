a332c1701afadf142f3e36711148f152df73fe1a
hzz <383686357@qq.com>
2023-09-28 21:37:18 +0800

    update

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaModule } from './prisma/prisma.module';
-import { UserModule } from './user/user.module';
-import { PermissionModule } from './permission/permission.module';
+import { PrismaModule } from './modules/prisma/prisma.module';
+import { UserModule } from './modules/user/user.module';
+import { PermissionModule } from './modules/permission/permission.module';
 
 @Module({
   imports: [PrismaModule, UserModule, PermissionModule],
--- packages/api/src/modules/permission/permission.service.ts
+++ packages/api/src/modules/permission/permission.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePermissionDto } from './dto/create-permission.dto';
 import { UpdatePermissionDto } from './dto/update-permission.dto';
-import { PrismaService } from '@/prisma/prisma.service';
+import { PrismaService } from '@/modules/prisma/prisma.service';
 import { PermissionType } from '@/types/enums';
 
 @Injectable()
